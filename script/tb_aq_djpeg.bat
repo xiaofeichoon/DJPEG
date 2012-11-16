ad6731edcb0fddb977d51433d7347d305678ddda
Hidemi Ishihara <hidemi@sweetcafe.jp>
2012-11-17 02:27:22 +0900

    modify for simulation bug.

--- script/tb_aq_djpeg.bat
+++ script/tb_aq_djpeg.bat
@@ -1,1 +1,1 @@
-fuse.exe -incremental -lib unisims_ver -lib unimacro_ver -lib xilinxcorelib_ver -o ./tb_aq_djpeg.exe -prj ../config/tb_aq_djpeg.prj work.tb_aq_djpeg work.glbl
+fuse -incremental -lib unisims_ver -lib unimacro_ver -lib xilinxcorelib_ver -o ./tb_aq_djpeg -prj ../config/tb_aq_djpeg.prj work.tb_aq_djpeg work.glbl
